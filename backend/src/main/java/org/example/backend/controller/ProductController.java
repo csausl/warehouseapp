0d6afd34f27e2f586d78d15856e9a62e43eca2f9
chris s <test@test.com>
2025-11-26 13:16:18 +0100

    removed example

--- backend/src/main/java/org/example/backend/controller/ProductController.java
+++ backend/src/main/java/org/example/backend/controller/ProductController.java
@@ -3,7 +3,6 @@ package org.example.backend.controller;
 import org.example.backend.model.dto.NewProductDTO;
 import org.example.backend.model.entities.Product;
 import org.example.backend.service.ProductService;
-import org.example.backend.utils.enums.Category;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -18,13 +17,6 @@ public class ProductController {
 
     public ProductController(ProductService productService) {
         this.productService = productService;
-    }
-
-    @GetMapping("/example")
-    public String example(){
-        return """
-                {"greeting":"hello chris"}
-                """;
     }
 
     @PostMapping("/product")

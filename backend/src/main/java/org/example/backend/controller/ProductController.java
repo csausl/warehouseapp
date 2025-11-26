7c21c8a83ec4f7741b231149042715409dd7beb4
chris s <test@test.com>
2025-11-26 11:34:09 +0100

    testing

--- backend/src/main/java/org/example/backend/controller/ProductController.java
+++ backend/src/main/java/org/example/backend/controller/ProductController.java
@@ -3,6 +3,7 @@ package org.example.backend.controller;
 import org.example.backend.model.dto.NewProductDTO;
 import org.example.backend.model.entities.Product;
 import org.example.backend.service.ProductService;
+import org.example.backend.utils.enums.Category;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -28,8 +29,8 @@ public class ProductController {
 
     @PostMapping("/product")
     public Product addProduct(@RequestBody NewProductDTO newProduct) {
-        System.out.println(newProduct );
-
+        //System.out.println(newProduct);
+        //NewProductDTO test=new NewProductDTO(newProduct.name(),newProduct.barcode(),newProduct.description(),newProduct.quantity(), Category.CLOTHING);
         return productService.addProduct(newProduct);
     }
 }

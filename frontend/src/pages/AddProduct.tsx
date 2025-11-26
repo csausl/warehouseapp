3ca85b1f98336fc1208a1b56deaa887f3c57e175
chris s <test@test.com>
2025-11-26 10:48:24 +0100

    added first frontend functions

--- frontend/src/pages/AddProduct.tsx
+++ frontend/src/pages/AddProduct.tsx
@@ -1,8 +1,23 @@
+import axios from "axios";
+import {useState} from "react";
+
 export default function AddProduct() {
+
+    const [greeting, setGreeting] = useState<string>("");
+
+    function handleGreet() {
+        axios.get("/api/example")
+            .then((response) => {
+                console.log(response.data);
+                setGreeting(response.data.greeting);})
+
+    }
 
     return (
         <>
-            <h1> Hello</h1>
+            <h1>Hello World!</h1>
+            <button onClick={handleGreet}>click me</button>
+            <h2>{greeting}</h2>
         </>
     )
 }

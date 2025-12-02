aa4b91a4962de444129ad5b801fed087c951394f
chris s <test@test.com>
2025-12-02 11:06:10 +0100

    testing for OAuth protected endpoints

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -31,9 +30,11 @@ public class SecurityConfig {
                         //.requestMatchers(HttpMethod.GET, "/api/dashboard").authenticated() // spezifischer Endpunkt zuerst gefiltert
                         .requestMatchers("/api/auth/me").authenticated() // anfrage hierauf geht nur wenn authentifiziert
                         .requestMatchers("/api/secured").authenticated()
+                        .requestMatchers("api/warehouse/example").authenticated()
                         .anyRequest().permitAll() // hierauf sind alle nicht authentifizierten anfragen ok - ERST AM ENDE!
                 )
                 .oauth2Login(o -> o.defaultSuccessUrl(appUrl)); // AppUrl enthält die Ziel-Seite nach Login OK
+                //.oauth2Login(o -> o.defaultSuccessUrl("http://localhost:5173")); // AppUrl enthält die Ziel-Seite nach Login OK
         return http.build();
     }
 }
--- backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -43,6 +43,7 @@ class ProductControllerTest {
     }
 
     @Test
+    @WithMockUser
     void example() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/api/warehouse/example"))
                 .andExpect(MockMvcResultMatchers.status().isOk())

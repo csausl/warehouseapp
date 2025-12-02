943ece13ebe933531ce8e3cb8c034aa773ee2cf8
chris s <test@test.com>
2025-12-02 09:57:07 +0100

    added oauth2

---
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.example.backend.security;
+
+
+//import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${app.url}")
+    private String appUrl;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // schaltet alle CSRF Checks aus - Großes NONO für Prod!
+                // wir filtern - ACHTUNG von SPEZIFISCH nach ALLGEMEIN, sonst gehen spezifische Blocks vielleicht verloren
+                // was zuerst gefiltert wird, überschreibt spätere engere Filter
+                .authorizeHttpRequests(a -> a //wir filtern welche Requests auf welche durchkommen
+                        // es lässt sich mit HttpMethod.GET / HttpMethod.POST.. auch auf spezielle Endpunkte filtern
+                        //.anyRequest().authenticated() // wenn ich für jede Anfrage eine Auth erwarte, komme ich nicht mal auf die Logion Seite
+                        //.requestMatchers(HttpMethod.GET, "/api/dashboard").authenticated() // spezifischer Endpunkt zuerst gefiltert
+                        .requestMatchers("/api/auth/me").authenticated() // anfrage hierauf geht nur wenn authentifiziert
+                        .requestMatchers("/api/secured").authenticated()
+                        .anyRequest().permitAll() // hierauf sind alle nicht authentifizierten anfragen ok - ERST AM ENDE!
+                )
+                .oauth2Login(o -> o.defaultSuccessUrl(appUrl)); // AppUrl enthält die Ziel-Seite nach Login OK
+        return http.build();
+    }
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,32 @@
 
 import './App.css'
 import AddProduct from "./pages/AddProduct.tsx";
+import axios from "axios";
+import {useState} from "react";
 
 function App() {
 
+    const [username, setUsername] = useState("");
 
-  return (
+    function login() {
+        const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080': window.location.origin
+        window.open(host + '/oauth2/authorization/github', '_self')
+    }
+
+    function getMe(){
+        axios.get('/api/auth/me').then((response) => {
+                //console.log(response.data);
+                setUsername(response.data);
+            }
+        )
+    }
+
+    return (
     <>
         <AddProduct/>
-
+        <button onClick={login}>login</button>
+        <button onClick={getMe}>GetMe</button>
+        <h1>Username: {username}</h1>
       <h1>hallo</h1>
 
     </>
